d23a28679e5a788c136b38258d4201aa9a68429f
DionatanPO <dionatanoliveirapc@gmail.com>
2020-04-12 11:53:00 -0300

    Atualizando 1.0.12 pc desk

---
+++ src/main/java/com/example/api_avicultura/service/PesagemService.java
@@ -0,0 +1,34 @@
+package com.example.api_avicultura.service;
+
+import com.example.api_avicultura.model.Pesagem;
+import com.example.api_avicultura.repository.PesagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PesagemService {
+
+    @Autowired
+    PesagemRepository pesagemRepository;
+
+    public Pesagem salvar(Pesagem pesagem) {
+        //validações e etcS
+        return pesagemRepository.save(pesagem);
+    }
+
+    public void delete(Pesagem pesagem) {
+
+        pesagemRepository.delete(pesagem);
+    }
+
+    public Pesagem buscaPorID(Long id) throws Exception {
+        return pesagemRepository.findById(id).orElseThrow(Exception::new);
+    }
+
+    public List buscarTodos() {
+        return pesagemRepository.findAll();
+    }
+
+}
